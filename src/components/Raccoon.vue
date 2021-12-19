3dc6faa2e9616932b2b9d293900ccd707eb0154f
Anouk <anoukvangrieken@gmail.com>
2021-12-19 23:28:06 +0100

    raccoon emotions

--- src/components/Raccoon.vue
+++ src/components/Raccoon.vue
@@ -20,16 +20,16 @@ Gestion des points d'alimentation au clic, si trop chiant de faire un drag and d
       <progress id="file" max="100" :value="counter"> {{ counter }} % </progress>
       <img alt="Raccoon cute" :src="raccoon_emotion" usemap=#cuteRaccoon>
       <map name="cuteRaccoon" id="cuteRaccoon">
-          <area shape="rect" alt="" title="Left ear" coords="155,68,225,159" href="#Raccoon" @click="change_raccoon_emotion('h', -20)"/>
-          <area shape="rect" alt="" title="Right ear" coords="340,38,417,102" href="#Raccoon" @click="change_raccoon_emotion('h', -20)" />
-          <area shape="rect" alt="" title="Nose" coords="358,199,399,230" href="#Raccoon" @click="change_raccoon_emotion('h', -20)" />
-          <area shape="rect" alt="" title="Left eye" coords="283,151,311,174" href="#Raccoon" @click="change_raccoon_emotion('h', -20)" />
-          <area shape="rect" alt="" title="Right eye" coords="366,141,385,165" href="#Raccoon" @click="change_raccoon_emotion('h', -20)"/>
-          <area shape="rect" alt="" title="Forehead" coords="236,80,360,142" href="#Raccoon" @click="change_raccoon_emotion('h', 20)" />
-          <area shape="rect" alt="" title="Left paw" coords="25,431,124,453" href="#Raccoon" @click="change_raccoon_emotion('h', -20)" />
-          <area shape="rect" alt="" title="Right paw" coords="411,447,492,477" href="#Raccoon" @click="change_raccoon_emotion('h', -20)" />
-          <area shape="rect" alt="" title="Belly" coords="206,277,376,444" href="#Raccoon" @click="change_raccoon_emotion('h', 20)" />
-          <area shape="rect" alt="" title="Shoulder" coords="413,302,486,392" href="#Raccoon" @click="change_raccoon_emotion('h', 20)" />
+          <area shape="rect" alt="" title="Left ear" coords="155,68,225,159" href="#Raccoon" @click="change_raccoon_emotion('angry', -10)"/>
+          <area shape="rect" alt="" title="Right ear" coords="340,38,417,102" href="#Raccoon" @click="change_raccoon_emotion('angry', -10)" />
+          <area shape="rect" alt="" title="Nose" coords="358,199,399,230" href="#Raccoon" @click="change_raccoon_emotion('sad', -20)" />
+          <area shape="rect" alt="" title="Left eye" coords="283,151,311,174" href="#Raccoon" @click="change_raccoon_emotion('sad', -20)" />
+          <area shape="rect" alt="" title="Right eye" coords="366,141,385,165" href="#Raccoon" @click="change_raccoon_emotion('sad', -20)"/>
+          <area shape="rect" alt="" title="Forehead" coords="236,80,360,142" href="#Raccoon" @click="change_raccoon_emotion('love', 20)" />
+          <area shape="rect" alt="" title="Left paw" coords="25,431,124,453" href="#Raccoon" @click="change_raccoon_emotion('vangry', -20)" />
+          <area shape="rect" alt="" title="Right paw" coords="411,447,492,477" href="#Raccoon" @click="change_raccoon_emotion('vangry', -20)" />
+          <area shape="rect" alt="" title="Belly" coords="206,277,376,444" href="#Raccoon" @click="change_raccoon_emotion('love', 20)" />
+          <area shape="rect" alt="" title="Shoulder" coords="413,302,486,392" href="#Raccoon" @click="change_raccoon_emotion('shy', 20)" />
       </map>
       <victorymodale :revele="reveleVictory" :toggleModale="toggleVictoryModale"></victorymodale>
       <loosemodale :revele="reveleLoose" :toggleModale="toggleLooseModale"></loosemodale>
@@ -40,8 +40,11 @@ Gestion des points d'alimentation au clic, si trop chiant de faire un drag and d
 <script>
 //change_raccoon_emotion('angry', -20)
 import neutral_raccoon from "../../img/raccoon.png"
-import happy_raccoon from "../../img/raccooncute.png"
-import angry_raccoon from "../../img/angryraccoon.jpg"
+import angry_raccoon from "../../img/angry_raccoon.png"
+import in_love_raccoon from "../../img/in_love_raccoon.png"
+import sad_raccoon from "../../img/sad_raccoon.png"
+import shy_raccoon from "../../img/shy_raccoon.png"
+import very_angry_raccoon from "../../img/very_angry_raccoon.png"
 import VictoryModale from "./VictoryModale";
 import LooseModale from "./LooseModale";
 //import sad_raccoon from "" //ATTENTION SI LE CHEMIN EST VIDE L'APPLI PLANTE
@@ -66,15 +69,21 @@ export default {
   methods : { //Fonctions utilisées dans le composant
     change_raccoon_emotion (emotion, n) { //Fonction qui change l'émotion du Raccoon, incrémente le compteur et gère la victoire
       switch(emotion) {
-        case ("happy"):
-          this.raccoon_emotion = happy_raccoon
+        case ("shy"):
+          this.raccoon_emotion = shy_raccoon
           break
         case ("angry") :
           this.raccoon_emotion = angry_raccoon
           break
-       // case ("sad") :
-       //   this.raccoon_emotion = sad_raccoon
-        //  break
+        case ("love") :
+          this.raccoon_emotion = in_love_raccoon
+          break
+        case ("sad") :
+          this.raccoon_emotion = sad_raccoon
+          break
+        case ("vangry") :
+          this.raccoon_emotion = very_angry_raccoon
+          break
         default :
           this.raccoon_emotion = neutral_raccoon
       }
