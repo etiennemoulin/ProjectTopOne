bb2cfb7ae81aa80616d21ca39139e501d881971b
Anouk <anoukvangrieken@gmail.com>
2021-12-19 16:52:47 +0100

    Ajouts de commentaires

--- src/components/Raccoon.vue
+++ src/components/Raccoon.vue
@@ -7,6 +7,12 @@ Appeler un élément de Data dans une balise html :
 Appeler un élément de Data dans un argument d'une balise :
 <progress id="file" max="100" :value="ma_variable">
 
+PROPOSITION DE PRESENTATION :
+
+BARRE DU PROGRESSION EN HAUT DE L'ECRAN
+RACCOON EN DESSOUS
+BARRE AVEC PROPOSITION DE NOURRITURE EN DESSOUS DU RACCON
+Gestion des points d'alimentation au clic, si trop chiant de faire un drag and drop
 -->
 <template>
   <div class="Raccoon">
@@ -25,6 +31,7 @@ Appeler un élément de Data dans un argument d'une balise :
           <area shape="rect" alt="" title="Belly" coords="206,277,376,444" href="#Raccoon" @click="increment(20)" />
           <area shape="rect" alt="" title="Shoulder" coords="413,302,486,392" href="#Raccoon" @click="increment(20)" />
       </map>
+    <p>ICI BARRE DE NOURRITURE, METTRE LES PHOTOS EN PETIT</p>
   </div>
 </template>
 
@@ -33,7 +40,7 @@ Appeler un élément de Data dans un argument d'une balise :
 import neutral_raccoon from "../../img/raccoon.png"
 import happy_raccoon from "../../img/raccooncute.png"
 import angry_raccoon from "../../img/angryraccoon.jpg"
-//import sad_raccoon from ""
+//import sad_raccoon from "" //ATTENTION SI LE CHEMIN EST VIDE L'APPLI PLANTE
 
 export default {
   name: 'Raccoon', //Nom du composant
